864aaec88c8e8c755b8e67e5c8d520f765793311
Lennart Hennigs <mail@lennarthennigs.de>
2023-05-22 21:08:37 +0200

    refactored code and fixed bug with parameters

--- src/pi_pico/code.py
+++ src/pi_pico/code.py
@@ -35,6 +35,7 @@ class KeyController:
         self.active_app = None
         self.last_key_config = None
 
+
     def open_folder(self, folder_name):
         if folder_name in self.folders:
             self.last_key_config = self.key_config
@@ -43,11 +44,13 @@ class KeyController:
             self.key_config = self.folders[folder_name]
             self.update_keys()
 
+
     def close_folder(self):
         self.folder_open = False
         self.key_config = self.last_key_config
         self.last_key_config = None
         self.update_keys()
+
 
     def key_action(self, key, press=True):
         if key.number in self.key_config:
@@ -67,15 +70,14 @@ class KeyController:
                 elif action == 'close_folder':
                     self.close_folder()
                 else:
-                    if isinstance(action, tuple):  # Check if action is a plugin command
-                        self.send_plugin_command(*action)  # If it is, call send_plugin_command
-                    else:
-                        self.handle_key_sequences(key_sequences, press)
-                        if key_config_dict.get('application'):
-                            app_name = key_config_dict['application']
-                            self.send_application_name(app_name)
-            else:
-                self.handle_key_sequences(key_sequences, press)
+                    if isinstance(action, tuple):
+                        self.send_plugin_command(*action)
+                    app_name = key_config_dict.get('application')
+                    if app_name:
+                        self.send_application_name(app_name)
+                        
+            self.handle_key_sequences(key_sequences, press)
+
 
     def update_key_led(self, key, color, press):
         if key.number in self.key_config:
@@ -88,6 +90,7 @@ class KeyController:
                 description = key_config_dict.get('description', '')
                 # print(f"Key {key.number} pressed: {description}")
 
+
     def handle_key_sequences(self, key_sequences, press):
         for item in key_sequences:
             if isinstance(item, float):
@@ -99,6 +102,7 @@ class KeyController:
             else:
                 self.keyboard.press(
                     item) if press else self.keyboard.release(item)
+
 
     def update_keys(self):
         for key in self.keys:
@@ -116,6 +120,7 @@ class KeyController:
                 self.keypad.on_press(key, lambda _, key=key: None)
                 self.keypad.on_release(key, lambda _, key=key: None)
 
+
     def read_serial_line(self):
         if usb_cdc.console.in_waiting > 0:
             raw_data = usb_cdc.console.readline()
@@ -124,6 +129,7 @@ class KeyController:
             except UnicodeDecodeError:
                 pass
         return None
+
 
     def send_application_name(self, app_name):
         try:
@@ -134,11 +140,13 @@ class KeyController:
             # print(f"Could not launch {app_name}: {e}\n")
             pass
 
+
     def send_plugin_command(self, plugin, command):
         try:
             usb_cdc.console.write(f"Run: {plugin}.{command}\n".encode('utf-8'))
         except Exception as e:
             pass
+
 
     def run(self):
         while True:
@@ -161,88 +169,84 @@ class KeyController:
                 time.sleep(0.1)
                 self.keypad.update()
 
-    def read_key_configs(self, json_filename):
-        def convert_keycode_string(keycode_string):
-            keycode_list = keycode_string.split('+')
-            keycodes = []
-            for key in keycode_list:
-                if key not in self.KEYCODE_MAPPING:
-                    raise ValueError(
-                        f"Unknown keycode constant: {key} in '{keycode_string}'")
-                keycodes.append(self.KEYCODE_MAPPING[key])
-            return tuple(keycodes)
 
-        def convert_color_string(color_string):
-            if color_string.startswith("#"):
-                return tuple(int(color_string[i:i+2], 16) for i in (1, 3, 5))
-            return (0, 0, 0)
+    def convert_keycode_string(self, keycode_string):
+        keycode_list = keycode_string.split('+')
+        keycodes = []
+        for key in keycode_list:
+            if key not in self.KEYCODE_MAPPING:
+                raise ValueError(
+                    f"Unknown keycode constant: {key} in '{keycode_string}'")
+            keycodes.append(self.KEYCODE_MAPPING[key])
+        return tuple(keycodes)
 
-        def convert_value(value):
-            if isinstance(value, str):
-                return convert_keycode_string(value)
-            return value
 
-        with open(json_filename, 'r') as json_file:
-            json_data = json.load(json_file)
+    def convert_color_string(self, color_string):
+        if color_string.startswith("#"):
+            return tuple(int(color_string[i:i+2], 16) for i in (1, 3, 5))
+        return (0, 0, 0)
 
+
+    def convert_value(self, value):
+        if isinstance(value, str):
+            return self.convert_keycode_string(value)
+        return value
+
+
+    def get_config_items(self, config):
+        key_sequence = config.get('key_sequence', [])
+        key_sequences = tuple(self.convert_value(v) for v in key_sequence) if isinstance(
+            key_sequence, list) else self.convert_keycode_string(key_sequence)
+        color_array = self.convert_color_string(config.get('color', ''))
+        description = config.get('description', '')
+        application = config.get('application', '')
+        action = config.get('action', '')
+        if action and '.' in action:
+            action = tuple(action.split('.',1))
+        folder = config.get('folder', '')
+        return key_sequences, color_array, description, application, action, folder
+
+
+    def process_key_definitions(self, json_data):
         key_configs = {}
-        folders = {}
         for app, configs in json_data["key_definitions"].items():
             key_configs[app] = {}
             for key, config in configs.items():
-                key_sequence = config.get('key_sequence', [])
-                key_sequences = tuple(convert_value(v) for v in key_sequence) if isinstance(
-                    key_sequence, list) else convert_keycode_string(key_sequence)
-                color_array = convert_color_string(config.get('color', ''))
-                description = config.get('description', '')
-                application = config.get('application', '')
-                action = config.get('action', '')
-                if action:
-                    if '.' in action:
-                        action = tuple(action.split('.'))
-                folder = config.get('folder', '')
-
+                key_sequences, color_array, description, application, action, folder = self.get_config_items(config)
                 if folder and folder not in json_data["folders"]:
                     print(
                         f"Error: Folder '{folder}' not found. Disabling key binding.")
-                    key_sequences = ()
-                    color_array = (0, 0, 0)
-                    description = ''
-                    application = ''
-                    action = ''
-                    folder = ''
-                else:
                     key_sequences, color_array, description, application, action, folder = (
-                        key_sequences, color_array, description, application, action, folder)
-
+                        (), (0, 0, 0), '', '', '', '')
                 key_configs[app][int(key)] = (
                     key_sequences, color_array, description, application, action, folder)
+        return key_configs
 
+
+    def process_folders(self, json_data):
+        folders = {}
         for folder_name, folder_configs in json_data["folders"].items():
             folders[folder_name] = {}
             close_folder_found = False
             for key, config in folder_configs.items():
-                key_sequence = config.get('key_sequence', [])
-                key_sequences = tuple(convert_value(v) for v in key_sequence) if isinstance(
-                    key_sequence, list) else convert_keycode_string(key_sequence)
-                color_array = convert_color_string(config.get('color', ''))
-                description = config.get('description', '')
-                application = config.get('application', '')
-                action = config.get('action', '')
-                folder = config.get('folder', '')
-
+                key_sequences, color_array, description, application, action, folder = self.get_config_items(config)
                 if action == "close_folder":
                     close_folder_found = True
-
                 folders[folder_name][int(key)] = (
                     key_sequences, color_array, description, application, action, folder)
-
             if not close_folder_found:
                 raise ValueError(
                     f"Error: Folder '{folder_name}' does not have a 'close_folder' action defined.")
+        return folders
 
+
+    def read_key_configs(self, json_filename):
+        with open(json_filename, 'r') as json_file:
+            json_data = json.load(json_file)
+        key_configs = self.process_key_definitions(json_data)
+        folders = self.process_folders(json_data)
         return key_configs, folders
-
+ 
 
 if __name__ == "__main__":
     controller = KeyController()
