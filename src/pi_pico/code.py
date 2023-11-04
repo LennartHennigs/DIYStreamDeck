4e866fe365635b8cce344747f18d09b2382250a3
Lennart Hennigs <mail@lennarthennigs.de>
2023-11-04 13:26:11 +0100

    more refactoring

--- src/pi_pico/code.py
+++ src/pi_pico/code.py
@@ -1,6 +1,6 @@
 # DIY Streamdeck code for a Pi Pico - CircuitPython
 # L. Hennigs and ChatGPT 4.0
-# last changed: 23-06-03
+# last changed: 23-11-04
 # https://github.com/LennartHennigs/DIYStreamDeck
 
 import time
@@ -22,28 +22,27 @@ class KeyController:
         Keycode) if not name.startswith("__")}
 
     def __init__(self, verbose=False):
-        self.verbose=verbose
+        self.verbose = verbose
         self.keypad = RgbKeypad()
-        self.keys = self.keypad.keys
-        self.autoclose_current_folder = False
-        self.urls = {}
-        self.global_config = {}
         self.keyboard = Keyboard(usb_hid.devices)
         self.layout = KeyboardLayoutUS(self.keyboard)
-        self.key_configs, self.folders, self.global_config, self.urls = self.read_key_configs(self.JSON_FILE)
+        self.keys = self.keypad.keys
+        self.autoclose_current_folder = False
+
+        self.json = self.parse_json(self.JSON_FILE)
+        self.global_config = self.process_global_section(self.json)
+        self.key_configs = self.process_app_section(self.json)
+        self.folders = self.process_folder_section(self.json)
+        self.urls = self.process_folder_section(self.json)
+
         self.current_config = self.key_configs.get("_otherwise", {})
-        self.usb_serial = usb_cdc.console
-        self.update_keys()
-        self.folder_open = False
-        self.active_app = None
-        self.last_key_config = None
         self.folder_stack = [] 
+        self.update_keys()
 
     
     def open_folder(self, folder_name):        
         if folder_name in self.folders:
             self.folder_stack.append(self.current_config)
-            self.folder_open = True
             self.current_config= self.folders[folder_name]
             self.autoclose_current_folder = self.current_config.get('autoclose', True)
             self.update_keys()
@@ -53,40 +52,35 @@ class KeyController:
         if not self.folder_stack:
             return
         self.current_config = self.folder_stack.pop()
-        # update the keys
         self.update_keys()
 
 
     def key_action(self, key, press=True):
-        # check if the key is in the current key config
         if key.number not in self.current_config:
             return
-        # get the key config for the key
         key_def = self.current_config[key.number]
-        # key is pressed
-        if press:
-            key.led_off()
-        # key is released
-        else:
+
+        action = key_def.get('action')
+        folder = key_def.get('folder')
+        app = key_def.get('application')
+        keys = key_def.get('key_sequences')
+        someAction = True
+        #  open a folder?
+        if folder:
+            self.open_folder(folder)
             someAction = False
-            #  open a folder?
-            if key_def.get('folder'):
-                self.open_folder(key_def.get('folder'))
-            # is the action is a plugin command
-            elif isinstance(key_def.get('action'), tuple):
-                self.send_plugin_command(*key_def.get('action'))
-                someAction = True
-            # open an application?
-            elif key_def.get('application'):
-                self.send_application_name(key_def.get('application'))
-                someAction = True
-            # handle the key sequences
-            elif key_def.get('key_sequences'):
-                self.handle_key_sequences(key_def.get('key_sequences'))
-                someAction = True
-            # close the folder
-            if (someAction and self.autoclose_current_folder) or key_def.get('action') == 'close_folder':
-                self.close_folder()      
+        # is the action is a plugin command
+        elif isinstance(action, tuple):
+            self.send_plugin_command(*action)
+        # open an application?
+        elif app:
+            self.send_application_name(app)
+        # handle the key sequences
+        elif keys:
+            self.handle_key_sequences(keys)
+        # close the folder
+        if (someAction and self.autoclose_current_folder) or action == 'close_folder':
+            self.close_folder()      
 
             
     def handle_key_sequences(self, key_sequences):
@@ -117,8 +111,8 @@ class KeyController:
             if key.number in self.current_config:
                 key.set_led(*self.current_config[key.number]['color']);
                 # Set the actual key event handlers
-                self.keypad.on_press(key, self.key_action)
-                self.keypad.on_release(key, lambda key=key: self.key_action(key, press=False))
+                self.keypad.on_press(key, lambda key=key: key.led_off())
+                self.keypad.on_release(key, lambda key=key: self.key_action(key))
 
 
     def read_serial_line(self):
@@ -171,7 +165,7 @@ class KeyController:
                 self.keypad.update()
 
 
-    def keycode_string_to_tupel(self, keycode_string):
+    def keycode_string_to_tuple (self, keycode_string):
         keycode_list = keycode_string.split('+')
         keycodes = []
         for key in keycode_list:
@@ -182,16 +176,23 @@ class KeyController:
         return tuple(keycodes)
 
 
-    def convert_color_string(self, color_string):
+    def color_string_to_tuple(self, color_string):
         if color_string.startswith("#"):
             return tuple(int(color_string[i:i+2], 16) for i in (1, 3, 5))
         else:
             return (0, 0, 0)
 
 
+    def convert_action_string(self, action):
+        if action and '.' in action:
+            return tuple(action.split('.',1))
+        else:
+            return action
+
+
     def convert_value(self, value):
         if isinstance(value, str):
-            return self.keycode_string_to_tupel(value)
+            return self.keycode_string_to_tuple (value)
         else:
             return value
 
@@ -199,21 +200,14 @@ class KeyController:
     def get_config_items(self, config):
         key_sequence = config.get('key_sequence', [])
         key_sequences = tuple(self.convert_value(v) for v in key_sequence) if isinstance(
-            key_sequence, list) else self.keycode_string_to_tupel(key_sequence)
-        color_array = self.convert_color_string(config.get('color', ''))
-        description = config.get('description', '')
-        application = config.get('application', '')
-        action = config.get('action', '')
-        if action and '.' in action:
-            action = tuple(action.split('.',1))
-        folder = config.get('folder', '')
+            key_sequence, list) else self.keycode_string_to_tuple (key_sequence)
         return {
             'key_sequences': key_sequences,
-            'color': color_array,
-            'description': description,
-            'application': application,
-            'action': action,
-            'folder': folder
+            'color': self.color_string_to_tuple(config.get('color', '')),
+            'description': config.get('description', ''),
+            'application': config.get('application', ''),
+            'action': self.convert_action_string(config.get('action', '')),
+            'folder': config.get('folder', '')
         }
 
 
@@ -241,7 +235,7 @@ class KeyController:
         return global_config
 
 
-    def process_app_section(self, json_data, global_config):
+    def process_app_section(self, json_data):
         key_configs = {}
         for app, configs in json_data["applications"].items():
             # print(f"Application {app} loaded.")
@@ -257,13 +251,13 @@ class KeyController:
                 else:                     
                     key_configs[app][int(key)] = config_items
             if not ignore_globals:
-                for key, config in global_config.items():
+                for key, config in self.global_config.items():
                     if int(key) not in key_configs[app]:
                         key_configs[app][int(key)] = config
         return key_configs
 
 
-    def process_folder_section(self, json_data, global_config):
+    def process_folder_section(self, json_data):
         folders = {}
         for folder_name, folder_config in json_data["folders"].items():
             folders[folder_name] = {}
@@ -294,16 +288,11 @@ class KeyController:
 
         return folders
 
-    
-    def read_key_configs(self, json_filename):
+
+    def parse_json(self, json_filename): 
         with open(json_filename, 'r') as json_file:
-            json_data = json.load(json_file)
-        global_config = self.process_global_section(json_data)
-        key_configs = self.process_app_section(json_data, global_config)
-        folders = self.process_folder_section(json_data, global_config)
-        urls = self.process_urls(json_data)
-        return key_configs, folders, global_config, urls
-    
+            return json.load(json_file)
+
 
 if __name__ == "__main__":
     controller = KeyController()
